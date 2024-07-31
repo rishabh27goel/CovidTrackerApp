f1ad8efcc741cf4456bdae0854b0421ce36295d7
Rishabh Goel <rishabhgoel2705@gmail.com>
2024-07-31 05:37:08 +0530

    formatted all the code files

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,37 +1,38 @@
-import React from 'react'
-import {
-    Card,
-    CardContent,
-    Typography
-} from '@material-ui/core';
-import './InfoBox.css'
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function InfoBox({title, active, isRed, cases, total, ...props}) {
-    
-    return (
-        <Card 
-        onClick={props.onClick}
-        className={`infoBox 
-            ${active && 'infoBox--selected'} 
-            ${isRed && 'infoBox--red' }
+function InfoBox({ title, active, isRed, cases, total, ...props }) {
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox 
+            ${active && "infoBox--selected"} 
+            ${isRed && "infoBox--red"}
             ${isRed && title === "Coronavirus Cases" && "infoBox--orange"}
-            `}>
-            <CardContent>
+            `}
+    >
+      <CardContent>
+        {/* Title i.e. Coronavirus cases */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {" "}
+          {title}{" "}
+        </Typography>
 
-                {/* Title i.e. Coronavirus cases */}
-                <Typography className="infoBox__title" color="textSecondary"> {title} </Typography>
+        {/* +120k Number of cases */}
+        <h2
+          className={`infoBox__cases ${
+            isRed && title === "Coronavirus Cases" && "infoBox__cases--orange"
+          } ${!isRed && "infoBox__cases--green"}`}
+        >
+          {cases}
+        </h2>
 
-                {/* +120k Number of cases */}
-                <h2 className={`infoBox__cases ${isRed && title === "Coronavirus Cases" && "infoBox__cases--orange"} ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
-
-                {/* 1.2M Total */}
-                <Typography className="infoBox__total">
-                    {total} Total
-                </Typography>
-
-            </CardContent>
-        </Card>
-    )
+        {/* 1.2M Total */}
+        <Typography className="infoBox__total">{total} Total</Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
--- src/components/util.js
+++ src/components/util.js
@@ -1,62 +1,59 @@
 import React from "react";
-import numeral from 'numeral';
-import {Circle, Popup} from 'react-leaflet';
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
 
 const casesTypeColors = {
-    cases: {
-        hex: "#FF8C00",
-        multiplier: 120,
-    },
-    recovered: {
-        hex: "#33d433",
-        multiplier: 130,
-    },
-    deaths: {
-        hex: "#fb4443",
-        multiplier: 800,
-    },
-}
+  cases: {
+    hex: "#FF8C00",
+    multiplier: 120,
+  },
+  recovered: {
+    hex: "#33d433",
+    multiplier: 130,
+  },
+  deaths: {
+    hex: "#fb4443",
+    multiplier: 800,
+  },
+};
 export const prettyPrintStats = (stat) =>
-    stat ? `+${numeral(stat).format("0.0a")}` :  "+0";
+  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
 
-
-export const sortData = (data) =>{
-    
-    const sortedData = [...data];
-
-    sortedData.sort((a, b) => (a.cases > b.cases) ? -1 : 1 );
-
-    return sortedData;
+export const sortData = (data) => {
+  const sortedData = [...data];
+  sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+  return sortedData;
 };
 
 // Draw circles on the map with interactive tooltip
-export const showDataOnMap = (data, casesType) => (
-    
-    data.map(country => (
-
-        <Circle
-        center = {[country.countryInfo.lat, country.countryInfo.long]}
-        fillOpacity = {0.4}
-        color= {casesTypeColors[casesType].hex}
-        fillColor= {casesTypeColors[casesType].hex}
-        
-        radius={
-            Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-        }
-        >
-        <Popup>
-           <div className="info-container">
-
-                <div className="info-flag" style ={{backgroundImage: `url(${country.countryInfo.flag})`}}/>
-                <div className="info-name">{country.country}</div>
-                <div className="info-confirmed">Cases: {numeral(country.cases).format("0,0") } </div>
-                <div className="info-recovered">Recovered: {numeral(country.recovered).format("0,0") } </div>
-                <div className="info-deaths">Deaths: {numeral(country.deaths).format("0,0") } </div>
-           </div>
-        </Popup>
-
-        </Circle>
-
-        
-    ))
- );
+export const showDataOnMap = (data, casesType) =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}{" "}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}{" "}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}{" "}
+          </div>
+        </div>
+      </Popup>
+    </Circle>
+  ));
